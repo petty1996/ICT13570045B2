4ab649bd4fd850615661e8608a59acb189b3f43f
ict <ict@last-row-colum2.local>
2017-12-21 18:13:16 +0700

    Init

---
+++ ICT13570045B2/ICT13570045B2Page.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace ICT13570045B2
+{
+    public partial class ICT13570045B2Page : ContentPage
+    {
+        public ICT13570045B2Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
